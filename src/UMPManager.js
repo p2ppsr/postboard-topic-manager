d879c7c0123452fee6cd0726a93150ce3c9873e2
Brayden Langley <brayden167@gmail.com>
2023-02-07 12:32:42 -0800

    Added output validation to the manager

--- src/UMPManager.js
+++ src/UMPManager.js
@@ -1,116 +1,103 @@
+const bsv = require('babbage-bsv')
+const pushdrop = require('pushdrop')
 /**
  * Responsible for managing the admission of UMP UTXOs
  */
+// Fields Template for Reference
+// // UMP Account Fields
+// Buffer.from(CWI_PROTOCOL_ADDRESS, 'utf8'),
+// Buffer.from(issuanceId, 'hex'), // Issuance txid and output index (faucet)
+// Buffer.from('01', 'hex'), // current UMP message (1 for new user) renditionId>
+// Buffer.from(passwordPresentationPrimary),
+// Buffer.from(passwordRecoveryPrimary),
+// Buffer.from(presentationRecoveryPrimary),
+// Buffer.from(passwordPrimaryPrivileged),
+// Buffer.from(presentationRecoveryPrivileged),
+// Buffer.from(presentationHash),
+// Buffer.from(passwordSalt),
+// Buffer.from(recoveryHash),
+// Buffer.from(presentationKeyEncrypted),
+// Buffer.from(recoveryKeyEncrypted),
+// Buffer.from(passwordKeyEncrypted)
+
+// let currentOutpoint = parsedTransaction.tx.h+Number(out.i).toString(16).padStart(8, '0')
+
 class UMPManager {
   /**
    * Returns the outputs from the UMP transaction that are admissible.
    * @param {Object} obj all params given in an object
    * @param {Array} [obj.previousUTXOs] UTXOs belonging to the current topic being spent as input in the transaction
-   * @param {Object} obj.parsedTransaction transaction containing outputs to admit into the current topic 
-   * @returns 
+   * @param {Object} obj.parsedTransaction transaction containing outputs to admit into the current topic
+   * @returns
    */
   identifyAdmissibleOutputs ({ previousUTXOs, parsedTransaction }) {
-    const outputs = []
+    try {
+      const outputs = []
 
-    // if (previousUTXOs) {
-    //   // existing UTXOs that should be updated / deleted
-    // }
+      // Validate params
+      if (!Array.isArray(parsedTransaction.outputs) || parsedTransaction.outputs.length < 1) {
+        throw new Error('Transaction outputs must be included as an array!')
+      }
+      if (!Array.isArray(parsedTransaction.inputs) || parsedTransaction.inputs.length < 1) {
+        throw new Error('An array of transaction inputs is required!') // TODO Update error format
+      }
 
-    // for (const output of parsedTransaction.outputs){
-    //   // Decode the UMP account fields
-    //   const result = pushdrop.decode({
-    //     script: output.outputScript,
-    //     fieldFormat: 'buffer'
-    //   })
+      // Try to decode and validate transaction outputs
+      for (const [i, output] of parsedTransaction.outputs.entries()) {
+        // Decode the UMP account fields
+        try {
+          const result = pushdrop.decode({
+            script: output.script.toHex(),
+            fieldFormat: 'buffer'
+          })
 
-    //   // UMP Account Fields (Why should these be decoded here if they are not returned to be stored in the db?)
-    //   const cwiProtocolAddress = result[0]
-    //   const issuanceId = result[1]
-    //   const currentUMPMessage = result[2]
-    //   const passwordPresentationPrimary = result[3]
-    //   const passwordRecoveryPrimary = result[4]
-    //   const presentationRecoveryPrimary = result[5]
-    //   const passwordPrimaryPrivileged = result[6]
-    //   const presentationHash = result[7]
-    //   const passwordSalt = result[8]
-    //   const recoveryHash = result[9]
-    //   const presentationKeyEncrypted = result[10]
-    //   const recoveryKeyEncrypted = result[11]
-    //   const recoverpasswordKeyEncryptedyHash = result[12]
+          // const OP_CHECKSIG = 172 // This check isn't needed right?
 
-    //   // TODO: Validate fields
-    // }
+          if (result.fields[0].toString() === '14HpZFLijstRS8H1P7b6NdMeCyH6HjeBXF') {
+          // Check if this is an update, or a new UMP token
+            if (result.fields[2].toString('hex') !== '01') {
+              const [previousUTXO] = previousUTXOs.filter(x => x.txid === output.prevTxid)
+              if (!previousUTXO) {
+                const e = new Error('Could not find UMP token to update!')
+                e.code = 'ERR_TOKEN_NOT_FOUND'
+                throw e
+              }
+              // TODO: Just check it matches?
+              const issuanceIdMatches = result.fields[1].toString('hex').includes(previousUTXO[0].txid)
+              if (!issuanceIdMatches) {
+                const e = new Error('Transaction does not spend some issuance output') // ?
+                e.code = 'ERR_INVALID_TX'
+                throw e
+              }
+            }
 
-    // if (!Array.isArray(parsedTransaction.out) || parsedTransaction.out.length < 1) {
-    //   throw new Error('Transaction outputs must be included as an array!')
-    // }
-    // if (!Array.isArray(parsedTransaction.in) || parsedTransaction.in.length < 1) {
-    //   throw new Error('An array of transaction inputs is required!')
-    // }
+            // Use ECDSA to verify signature
+            const hasValidSignature = bsv.crypto.ECDSA.verify(
+              bsv.crypto.Hash.sha256(Buffer.concat(result.fields)),
+              bsv.crypto.Signature.fromString(result.signature),
+              bsv.PublicKey.fromString(result.lockingPublicKey)
+            )
+            if (!hasValidSignature) {
+              throw new Error('Invalid Signature')
+            }
+            outputs.push(i)
+          }
+        } catch (error) {
+          // Probably not a PushDrop token
+          console.error(error)
+        }
+      }
+      if (outputs.length === 0) {
+        throw new Error(
+          'This transaction does not publish a valid CWI account descriptor!'
+        )
+      }
 
-    // // Get a CWI account descriptor
-    // const OP_CHECKSIG = 172
-    // const out = parsedTransaction.out.filter(x =>
-    //   x.b1 && x.b1.op === OP_CHECKSIG &&
-    //   x.s2 === '14HpZFLijstRS8H1P7b6NdMeCyH6HjeBXF' &&
-    //   x.s16 !== undefined
-    // )[0]
-    // if (!out) {
-    //   throw new Error(
-    //     'This transaction does not publish a valid CWI account descriptor!'
-    //   )
-    // }
-
-    // const previousOutpoint = out.h3
-    // const previousTXID = previousOutpoint.slice(0, 64)
-    // const previousVout = parseInt(previousOutpoint.slice(64), 16)
-    // if (!action.in.some(x =>
-    //   x.e.h === previousTXID && x.e.i === previousVout
-    // )) {
-    //   throw new Error('Transaction does not spend some issuance output')
-    // }
-
-    // Use ECDSA to verify signature (NOTE: Is this optimal? Similar code but different implementation in CoolCert)
-    // const contains_char = (string, char) => (string.indexOf(char) > -1)
-    // let fields = Object.entries(out)
-    //   .filter(([key, _]) => (
-    //     contains_char(key, 'b') &&
-    //     Number(key.slice(1)) >= 2 &&
-    //     Number(key.slice(1)) < 16)
-    //   )
-    //   .map(([_, value]) =>
-    //     (value.op)
-    //       ? Buffer.from((value.op-80).toString(16).padStart(2, '0'), 'hex')
-    //       : Buffer.from(value, 'base64')
-    //   )
-    // const has_valid_signature = bsv.crypto.ECDSA.verify(
-    //   bsv.crypto.Hash.sha256(Buffer.concat(fields)),
-    //   bsv.crypto.Signature.fromString(out.h16),
-    //   bsv.PublicKey.fromString(out.h0)
-    // )
-    // if (!has_valid_signature) {
-    //   throw new Error('Invalid Signature')
-    // }
-
-    // let currentOutpoint = parsedTransaction.tx.h+Number(out.i).toString(16).padStart(8, '0')
-    // const new_account_record = {
-    //   currentRendition:               out.b4.op - 80,   // Rendition ID (starts at 1, increments by 1 with every update)
-    //   passwordPresentationPrimary:    out.b5,           // Primary Key encrypted with the XOR of the password key and the presentation key
-    //   passwordRecoveryPrimary:        out.b6,           // Primary Key encrypted with the XOR of the password key and the recovery key
-    //   presentationRecoveryPrimary:    out.b7,           // Primary Key encrypted with the XOR of the presentation key and the recovery key
-    //   passwordPrimaryPrivileged:      out.b8,           // Privileged Key encrypted with the XOR of the password key and the primary key
-    //   presentationRecoveryPrivileged: out.b9,           // Privileged Key encrypted with the XOR of the presentation key and the recovery key
-    //   presentationHash:               out.b10,          // The SHA-256 hash of the current presentation key
-    //   passwordSalt:                   out.b11,          // 32-byte password salt for use in PBKDF2
-    //   recoveryHash:                   out.b12,          // The SHA-256 hash of the current recovery key
-    //   presentationKeyEncrypted:       out.b13,          // Current presentation key encrypted with the Privileged Key
-    //   recoveryKeyEncrypted:           out.b14,          // Current recovery key encrypted with the Privileged Key
-    //   passwordKeyEncrypted:           out.b15,          // Current password key encrypted with the Privileged Key
-    //   signature:                      out.b16,          // A signature from the field 0 public key over fields 2-15
-    // }
-
-    // Returns an array of output numbers
-    return [0] // vout === ?
+      // Returns an array of output numbers
+      return outputs
+    } catch (error) {
+      return [0] // TODO: remove temp code
+    }
   }
 }
 module.exports = UMPManager
